d0bbedc1ff005d2b9a13bbd23a7327647993a176
Sherma'n Wilson <sherman.a.wilson@gmail.com>
2021-06-02 11:23:32 -0500

    Finished Exercise 0

--- code.js
+++ code.js
@@ -1,4 +1,10 @@
-// helloWorld function 
-function helloWorld() {
+// helloWorld function
+const helloWorld = function() {
+//function helloWorld() {
     return "Hello, World!";
+    //console.log("Hello, World!");
+}
+
+function sayHello() {
+
 }
